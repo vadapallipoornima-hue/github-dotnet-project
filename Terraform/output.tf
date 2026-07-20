bca5317a5b27eac639d22fd1d58847f5d89371da
poornima <poornima@gmail.com>
2026-07-20 13:52:00 -0400

    added terraform files

---
+++ Terraform/output.tf
@@ -0,0 +1,7 @@
+output "webapp_name" {
+    value = azurerm_windows_web_app.webapp.name
+}
+
+output "webapp_url" {
+    value = azurerm_windows_web_app.webapp
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "rg" {
+    type = string
+    default = "ucrg"
+}
+variable "location" {
+    type= string
+    default = "canadacentral"
+}
+variable "asp" {
+    type= string
+    default = "ucasp"
+}
+variable "webapp" {
+    type= string
+    default = "ucwebapp20"
+}
+
